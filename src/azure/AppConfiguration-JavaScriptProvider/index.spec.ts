9fa70023352b7b07d0e2471249b86ea05c646ee7
earloc <earloc@live.de>
2024-11-30 19:05:30 +0100

    test: add more versions

--- src/azure/AppConfiguration-JavaScriptProvider/index.spec.ts
+++ src/azure/AppConfiguration-JavaScriptProvider/index.spec.ts
@@ -1,7 +1,14 @@
 import nock from 'nock';
 
-import * as v1_0_0_preview4 from '@azure/app-configuration-provider-1_0_0_preview_4';
 import * as v1_1_2 from '@azure/app-configuration-provider-1_1_2';
+import * as v1_1_0 from '@azure/app-configuration-provider-1_1_0';
+import * as v1_0_1 from '@azure/app-configuration-provider-1_0_1';
+import * as v1_0_0 from '@azure/app-configuration-provider-1_0_0';
+import * as v1_0_0_preview4 from '@azure/app-configuration-provider-1_0_0_preview_4';
+import * as v1_0_0_preview3 from '@azure/app-configuration-provider-1_0_0_preview_3'; // refresh was introduced here, older versions incompatible from here
+
+
+
 import { createOptions, delay, IRefreshable } from '.';
 
 type loadDelegate = (connectionString: string, options?: any) => Promise<IRefreshable>;
@@ -44,7 +51,11 @@ describe('refresh', () => {
 
   test.each([
       ['v1.1.2', v1_1_2.load],
-      ['v1.0.0-preview4', v1_0_0_preview4.load] 
+      ['v1.1.0', v1_1_0.load],
+      ['v1.0.1', v1_0_1.load],
+      ['v1.0.0', v1_0_0.load],
+      ['v1.0.0-preview4', v1_0_0_preview4.load],
+      ['v1.0.0-preview3', v1_0_0_preview3.load],
     ])
     ('honors cooldown in %p', 
     async (_, load: loadDelegate) => {
